46d2805c0c6b7f0f0de64767a3dda539893a66e1
iamfinnz <ilham19ti@mahasiswa.pcr.ac.id>
2022-07-08 21:19:09 +0700

    Assignment Operator

---
+++ Assignment Operator/index.py
@@ -0,0 +1,18 @@
+# assignment operator
+
+age = 20
+age2 = 20
+
+age = age / 2
+# atau
+age2 /= 2
+
+print(age2)
+
+# contoh string
+firstName = "Ilham"
+lastName = "Arifin"
+
+completeName = firstName + " " + lastName
+
+print(completeName)
