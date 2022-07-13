73618714046e75739bded511f42cc8c13644b922
iamfinnz <ilham19ti@mahasiswa.pcr.ac.id>
2022-07-13 19:30:34 +0700

    Operator Perbandingan

---
+++ Operator Perbandingan/index.py
@@ -0,0 +1,13 @@
+# comparison (perbandingan) operator
+
+# toko online
+
+# dompet digital
+dompet = 1500004
+
+# membeli macbook
+total = 300000
+
+# validasi dompet sebelum sukses
+
+print(dompet >= total)
---
+++ Operator Perbandingan/index2.py
@@ -0,0 +1,13 @@
+# comparison (perbandingan) operator
+
+# toko online
+
+# login function
+
+username = "ilham"
+password = "123aaa123"
+
+# proses checking di database
+usernameYangAdaDiDatabase = "ilham cganteng"
+
+print(username == usernameYangAdaDiDatabase)
