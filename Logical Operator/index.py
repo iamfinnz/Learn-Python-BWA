e52b39198a82c5527a6cff162129a5384faf32fe
iamfinnz <ilham19ti@mahasiswa.pcr.ac.id>
2022-07-14 19:14:27 +0700

    Logical Operator

---
+++ Logical Operator/index.py
@@ -0,0 +1,10 @@
+# logical operator
+# mengkombinasikan suatu statement
+
+# register user baru
+password = "123ilham"
+confirmPassword = "123ilhan"
+
+# membuat validasi apakah confirmPassword sama 
+# dengan password yang ditulis sebelumnya pada form register
+print(password == "123ilham" and confirmPassword == password)
---
+++ Logical Operator/index2.py
@@ -0,0 +1,8 @@
+# logical operator
+# mengkombinasikan suatu statement
+
+# check kelayakan kredit bank si Budi
+isKerja = False
+isUsaha = True
+
+print(isKerja == True or isUsaha == True)
