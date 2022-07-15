85408441008c66b5235231cc9607d3dfc0f71e15
iamfinnz <ilham19ti@mahasiswa.pcr.ac.id>
2022-07-15 18:58:56 +0700

    New Data

---
+++ New Data/index.py
@@ -0,0 +1,8 @@
+# list data type
+# sebuah tipe data yang bisa diubah (mutable)
+
+# 3 item mobil = 0, 1, 2 untuk index
+cars = ["bmw", "honda", "porsche", "toyota", "renault", "nissan"]
+
+# menampilkan data secara rapih
+print(cars[2:6])
