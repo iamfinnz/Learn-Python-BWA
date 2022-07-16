2ffa61736985155a7b73b5dd705980d7ab3c60f6
iamfinnz <ilham19ti@mahasiswa.pcr.ac.id>
2022-07-16 19:17:30 +0700

    Panjang Item List

---
+++ Panjang Item List/index.py
@@ -0,0 +1,13 @@
+# list data type
+# sebuah tipe data yang bisa kita ubah (mutable)
+
+# 3 item mobil = 0, 1, 2 untuk index
+cars = ["bmw", "honda", "prosche", "toyota", "renault", 
+        "nissan", "mitsubishi"]
+
+houses = ["ciputra", "lippo", "meikarta"]
+# menghitung panjang item dari list yang ada
+print(len(cars))
+print(len(houses))
+
+print(len(houses[1]))
