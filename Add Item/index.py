b6688adeab0300c50d2ead62225ed043c87e974c
iamfinnz <ilham19ti@mahasiswa.pcr.ac.id>
2022-07-17 08:49:51 +0700

    Add Item

---
+++ Add Item/index.py
@@ -0,0 +1,14 @@
+# list data type
+# sebuah tipe data yang bisa kita ubah (mutable)
+
+# 3 items food
+foods = ["ayam", "ikan", "sayur"]
+
+foods.append("nasi")
+
+print(foods)
+
+# merubah nasi menjadi kentang atau ubi
+foods[3] = "kentang"
+
+print(foods)
